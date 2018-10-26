017520e19e1442c0f10883d361dbe269a5ce1f44
yan7592842 email = 290388252@qq.com [core] <290388252@qq.com>
2018-10-26 10:30:30 +0800

    [Change] shareGzh url

--- src/app/publicSystem/shareGzh/share.component.ts
+++ src/app/publicSystem/shareGzh/share.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AppService} from '../../app-service';
 import {AppProperties} from '../../app.properties';
-
+declare var wx: any;
 import {urlParse} from '../../utils/util';
 @Component({
   selector: 'app-share',
@@ -18,12 +18,63 @@ export class ShareComponent implements OnInit {
   }
   ngOnInit() {
     this.token = urlParse(window.location.href)['token'];
+    this.share();
     this.appService.postAliData(this.appProperties.adminCreateForeverStrQrUrl, '', this.token).subscribe(
       data => {
         console.log(data);
         if (data.code === 0) {
           this.img = data.data;
         }
+      },
+      error2 => {
+        console.log(error2);
+      }
+    );
+  }
+  share() {
+    this.appService.postAliData(this.appProperties.wechatShareInfoUrl + '?url=http://sms.youshuidaojia.com/shareGzh?token=' + this.token,
+      '', this.token).subscribe(
+      data => {
+        console.log(data);
+        wx.config({
+          debug: false,
+          appId: data.data.appId,
+          timestamp: data.data.timestamp,
+          nonceStr: data.data.nonceStr,
+          signature: data.data.signature,
+          jsApiList: ['checkJsApi',
+            'onMenuShareAppMessage',
+            'onMenuShareTimeline',
+            'onMenuShareQQ',
+            'onMenuShareWeibo',
+          ]
+        });
+        const link = 'http://sms.youshuidaojia.com/share?token=' + this.token;
+        console.log(link);
+        wx.ready(function () {
+          const shareData = {
+            title: '优水到家',
+            desc: '分享领取优惠', // 这里请特别注意是要去除html
+            link: link,
+            imgUrl: 'http://119.23.233.123:6662/ys_admin/companyLogo/20181008_142714.png',
+            // imgUrl: '../../../assets/main/logo.png',
+            success: function () {
+              // 用户确认分享后执行的回调函数
+              console.log('success');
+            },
+            cancel: function () {
+              // 用户取消分享后执行的回调函数
+              console.log('cancel');
+            }
+          };
+          wx.onMenuShareAppMessage(shareData);
+          wx.onMenuShareTimeline(shareData);
+          wx.onMenuShareQQ(shareData);
+          wx.onMenuShareWeibo(shareData);
+        });
+        wx.error(function (res) {
+          console.log(res);
+        });
       },
       error2 => {
         console.log(error2);
